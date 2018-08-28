44a603aaf768e2b506511eb8dc20b8088abdddf3
Andy Parkinson <andy@envisialearning.com>
2018-08-28 11:16:15 -0700

    routing examples

--- config/routes/sample_client.rb
+++ config/routes/sample_client.rb
@@ -1,13 +1,15 @@
 #Edits require server restart to take effect
 Rails.application.routes.draw do
 
+
+  get 'sample_client/new_tool/introduction' => "custom/sample_client/new_tool/data_collection#introduction", as: :sample_client_new_tool_introduction
+
   namespace :custom do
     namespace :sample_client do
 
 
       namespace :new_tool do
-        get 'data_collection/introduction', as: :sample_client_new_tool_introduction
-        get 'data_collection/instructions', as: :sample_client_new_tool_instructions
+        get 'data_collection/instructions'
         get 'data_collection/questionaire'
         get 'data_collection/contra_indicator'
         get 'data_collection/contra_indicator_introduction'
@@ -17,8 +19,6 @@ Rails.application.routes.draw do
         get 'data_collection/thank_you_confirmation'
         get 'data_collection/thank_you'
       end
-
-
     end
   end
 end
