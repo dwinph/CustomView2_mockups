f22df9a076451f5ac479d75b13e3f6b8edf301fa
Natasha Cole <natasha@envisialearning.com>
2018-09-07 18:13:50 +0100

    Mockups updates

--- config/routes/heidrick_and_struggles.rb
+++ config/routes/heidrick_and_struggles.rb
@@ -1,10 +1,11 @@
  #Edits require server restart to take effect
 Rails.application.routes.draw do
 
-   get 'heidrick_and_struggles/taq_standard/index' => "custom/heidrick_and_struggles/taq_standard/data_collection#index", as: :heidrick_and_struggles_taq_standard_index
    get 'heidrick_and_struggles/taq_standard/introduction' => "custom/heidrick_and_struggles/taq_standard/data_collection#introduction", as: :heidrick_and_struggles_taq_standard_introduction
-   get 'heidrick_and_struggles/taq_standard/dropdown_questions' => "custom/heidrick_and_struggles/taq_standard/data_collection#dropdown_questions", as: :heidrick_and_struggles_taq_standard_dropdown_questions
-   get 'heidrick_and_struggles/taq_standard/table_questionnaire' => "custom/heidrick_and_struggles/taq_standard/data_collection#table_questionnaire", as: :heidrick_and_struggles_taq_standard_table_questionnaire
+   get 'heidrick_and_struggles/taq_standard/about_you' => "custom/heidrick_and_struggles/taq_standard/data_collection#about_you", as: :heidrick_and_struggles_taq_standard_about_you
+   get 'heidrick_and_struggles/taq_standard/about_you_questions' => "custom/heidrick_and_struggles/taq_standatd/data_collection#about_you_questions", as: :heidrick_and_struggles_about_you_questions
+   get 'heidrick_and_struggles/taq_standard/drive_factors_introduction' => "custom/heidrick_and_struggles/taq_standard/data_collection#drive_factors_introduction", as: :heidrick_and_struggles_taq_standard_drive_factors_introduction
+   get 'heidrick_and_struggles/taq_standard/drive_factors' => "custom/heidrick_and_struggles/taq_standard/data_collection#drive_factors", as: :heidrick_and_struggles_taq_standard_drive_factors
    get 'heidrick_and_struggles/taq_standard/drag_factors_introduction' => "custom/heidrick_and_struggles/taq_standard/data_collection#drag_factors_introduction", as: :heidrick_and_struggles_taq_standard_drag_factors_introduction
    get 'heidrick_and_struggles/taq_standard/drag_factors_questionnaire' => "custom/heidrick_and_struggles/taq_standard/data_collection#drag_factors_questionnaire", as: :heidrick_and_struggles_taq_standard_drag_factors_questionnaire
    get 'heidrick_and_struggles/taq_standard/organizational_context_introduction' => "custom/heidrick_and_struggles/taq_standard/data_collection#organizational_context_introduction", as: :heidrick_and_struggles_taq_standard_organizational_context_introduction
