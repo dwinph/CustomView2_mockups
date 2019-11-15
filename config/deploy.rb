89954aeaf4417a314bdbf80a296d9a496bf77c55
Aldwin Ibuna <aldwin@apemangroup.com>
2019-11-15 11:28:26 +0800

    Capistrano setup and files.

---
+++ config/deploy.rb
@@ -0,0 +1,41 @@
+# config valid only for Capistrano 3.1
+lock '3.4.0'
+
+# set :application, 'my_app_name'
+set :repo_url, "git-chromaticconcepts@pl1.projectlocker.com:CustomView2_mockups.git"
+set :deploy_via, :remote_cache
+
+set :linked_files , %w{config/database.yml config/application.yml}
+# set :linked_dirs, %w{public/xml public/attachments public/tmp tmp log}
+# set :whenever_identifier, ->{ "#{fetch(:application)}" }
+
+# set :whenever_roles, ["cron_production", "cron_staging"]
+
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:db), in: :groups, limit: 3, wait: 10 do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, 'db:sunrise'
+          # invoke 'delayed_job:restart'
+        end
+      end
+    end
+  end
+
+end
+
+# require './config/boot'
+# require 'airbrake/capistrano'
---
+++ config/deploy/staging.rb
@@ -0,0 +1,28 @@
+set :application, 'mockups.assessmenthub.net'
+
+set :deploy_to, "/Users/cv2mockups/sites/mockups.assessmenthub.net"
+set :branch, 'staging'
+set :rvm_type, :user 
+set :rvm_ruby_version, 'ruby-2.3.8'
+
+# Delayed job settings
+# role :worker, "jobs1.coreix.elsrv.com"
+# set :delayed_job_server_role, :worker
+# set :delayed_job_args, "#{YAML.load_file(File.expand_path("./config/delayed_job.yml"))[fetch(:stage).to_s]}"
+
+
+
+server  "staging1.coreix.elsrv.com", user: "cv2mockups", roles: [ 'web', 'app', 'db' ]
+
+set :linked_dirs, %w{tmp log public/uploads}
+
+namespace :deploy do
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within current_path do
+        # execute :bundle, 'exec sidekiqctl stop tmp/pids/sidekiq.pid'      
+      # end
+    end
+  end
+end
