e7e569fd0e2a82b86e59a01f7017a025ad5326b9
Erica <ask.ericavillena@gmail.com>
2020-04-14 16:39:10 +0800

    radio btns

--- app/assets/javascripts/custom/heidrick_and_struggles/rising_leaders_ifa/questionnaire/app.js
+++ app/assets/javascripts/custom/heidrick_and_struggles/rising_leaders_ifa/questionnaire/app.js
@@ -3,5 +3,13 @@ $(document).ready(function() {
 		    $( "#sortable" ).sortable();
 		    $( "#sortable" ).disableSelection();
 		}
+		$(".radio-item input").change(function(){
+			if ($(this).is(':checked')) {
+				$('.custom_rad_val').css("z-index",  1);
+				$('.custom_rad_val').css("color", "#444444");
+				$(this).parent().parent().children('.custom_rad_val').css("z-index",  99);
+				$(this).parent().parent().children('.custom_rad_val').css("color", "#FFFFFF");
+			}
+		});
 });
 
