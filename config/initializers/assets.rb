7cb19c9866cb009811e5c2e35e23394acab394de
Erica <ask.ericavillena@gmail.com>
2020-08-03 20:20:56 +0800

    slices for quintet

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -36,3 +36,8 @@ Rails.application.config.assets.precompile += %w( custom/heidrick_and_struggles/
 
 Rails.application.config.assets.precompile += %w( custom/king_personal_impact_experience/assessment/survey/app.js )
 Rails.application.config.assets.precompile += %w( custom/king_personal_impact_experience/assessment/survey/style.css )
+
+Rails.application.config.assets.precompile += %w( custom/quintet/richer_life/questionnaire/app.js )
+Rails.application.config.assets.precompile += %w( custom/quintet/richer_life/questionnaire/range.css )
+Rails.application.config.assets.precompile += %w( custom/quintet/richer_life/questionnaire/helper-classes.css )
+Rails.application.config.assets.precompile += %w( custom/quintet/richer_life/questionnaire/style.css )
